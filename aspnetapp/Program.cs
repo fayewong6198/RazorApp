8dc62df630bc357c8165106133cdef80fff55244
faye wong <trandaosimanh@gmail.com>
2021-09-09 20:58:14 +0700

    000

--- aspnetapp/Program.cs
+++ aspnetapp/Program.cs
@@ -6,14 +6,27 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+using aspnetapp.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
 namespace aspnetapp
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+                var host = CreateHostBuilder(args).Build();
+
+                using (var scope = host.Services.CreateScope())
+                {
+                    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                    db.Database.Migrate();
+                }
+
+                host.Run();
+
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
